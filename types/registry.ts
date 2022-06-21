97095fcf7555c75b1882a83d75489e1f0ef3de92
interc0der <intercoder@whirled.io>
2022-06-21 01:07:38 -0700

    Create: Types for registry json

---
+++ types/registry.ts
@@ -0,0 +1,32 @@
+export interface Initiative {
+  title: string;
+  description: string;
+  tag: number;
+  XAddress: string;
+  start: number;
+  end: number;
+}
+
+export interface RegistryEntry {
+  type: 'registry';
+  name: string;
+  description: string;
+  address: string;
+  image: string;
+  url: string;
+  phone: string;
+  mailingAddress: string;
+  country: string;
+  EIN: string;
+  initatives: Initiative[];
+}
+
+export interface HeadEntry {
+  type: 'head';
+  title: string;
+  description: string;
+}
+
+export type RegistryArray = { registry: RegistryEntry[] };
+export type Head = { document: HeadEntry };
+export type Registry = (Head | RegistryArray)[];
