d48f7d034fcc05c6374be8e7b30e01ffa331441f
interc0der <intercoder@whirled.io>
2022-06-20 22:41:17 -0700

    Create: types folder for typescript

---
+++ types/index.ts
@@ -0,0 +1,3 @@
+import * as Sample from './sample';
+
+export { Sample };
---
+++ types/sample.ts
@@ -0,0 +1,6 @@
+export interface Sample {
+  type: 'sample';
+  name: string;
+}
+
+export type SampleType = { Sample: Sample };
