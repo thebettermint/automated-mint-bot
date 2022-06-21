939615109dbef1c366941f677fb09b06c387e132
interc0der <intercoder@whirled.io>
2022-06-21 01:04:26 -0700

    Update: Add core functionality to lookup details in registry

--- src/lib/registry.ts
+++ src/lib/registry.ts
@@ -1,37 +1,69 @@
-import fs from 'fs';
+/* import fs from 'fs'; */
+import { spawnSync } from 'child_process';
+/* import registryJson from "@thebettermint/registry/src/registry.json" */
+import { Registry, RegistryArray, Head, RegistryEntry } from '../../types';
 
 /**
+ * registryOrganizations
  *
+ * @param {string} json - Total registry including document head
  *
- *
+ * @return { { name, ein }[] } - Array of organization name and ein
  */
-export const readRegistry = () => {
-  let registryJSON = fs.readFileSync(
-    '@thebettermint/registry/src/registry.json'
-  );
-  return registryJSON;
+export const getRegistryOrganizations = (json: Registry) => {
+  const registry = json
+    .map((obj: RegistryArray | Head) => {
+      if ('registry' in obj) return obj.registry;
+      return undefined;
+    })
+    .filter(Boolean);
+
+  if (!registry || registry[0] === undefined) return;
+
+  let parsedArray = registry[0]
+    .map((org: RegistryEntry | undefined) => {
+      if (!org) return;
+      return { name: org.name, ein: org.EIN };
+    })
+    .filter(Boolean);
+
+  return parsedArray;
 };
 
 /**
+ * getOrganizationAddress
  *
+ * @param {string} json - Total registry including document head
+ * @param {string} ein - Employer identifcation number of organization
  *
- *
+ * @return {string | Error} - Address if organization is found in registry
  */
-export const parseRegistry = () => {};
+export const getOrganizationAddress = (json: Registry, ein: string) => {
+  try {
+    const registry = json
+      .map((obj: RegistryArray | Head) => {
+        if ('registry' in obj) return obj.registry;
+        return undefined;
+      })
+      .filter(Boolean);
 
-/**
- *
- *
- *
- */
-export const registryOrganizations = () => {};
+    if (!registry || registry[0] === undefined) return;
 
-/**
- *
- *
- *
- */
-export const getOrganizationAddress = () => {};
+    let filtered = registry[0].filter((org: RegistryEntry | undefined) => {
+      ein === org?.EIN;
+    });
+
+    if (filtered.length > 1)
+      throw Error(
+        'Duplicate organizations with the same ein in registry. Please refer to repo and contact package maintainer'
+      );
+
+    let address = filtered[0].address;
+    return address;
+  } catch (error: any) {
+    return Error(error.msg);
+  }
+};
 
 /**
  *
@@ -40,18 +72,124 @@ export const getOrganizationAddress = () => {};
  */
 export const getOrganizationInitiative = () => {};
 
-export const getOrganizationDetailsByEIN = () => {};
+/**
+ * getOrganizationDetailsByEIN
+ *
+ * @param { Registry } json - Total registry including document head
+ * @param { string } ein - Employer identifcation number of organization
+ *
+ * @return { RegistryEntry } - Organization's registry entry
+ */
+export const getOrganizationDetailsByEIN = (json: Registry, ein: string) => {
+  try {
+    const registry = json
+      .map((obj: RegistryArray | Head) => {
+        if ('registry' in obj) return obj.registry;
+        return undefined;
+      })
+      .filter(Boolean);
+
+    if (!registry || registry[0] === undefined) return;
+
+    let filtered = registry[0].filter((org: RegistryEntry | undefined) => {
+      ein === org?.EIN;
+    });
+
+    if (filtered.length > 1)
+      throw Error(
+        'Duplicate organizations with the same ein in registry. Please refer to repo and contact package maintainer'
+      );
+
+    let org = filtered[0];
+    return org;
+  } catch (error: any) {
+    return Error(error.msg);
+  }
+};
+
+/**
+ * getEINArrayRegistry
+ *
+ * @param {string} json - Total registry including document head
+ *
+ * @return { string[] } - Array of registry ein's
+ */
+export const getEINArrayRegistry = (json: Registry) => {
+  try {
+    const registry = json
+      .map((obj: RegistryArray | Head) => {
+        if ('registry' in obj) return obj.registry;
+        return undefined;
+      })
+      .filter(Boolean);
+
+    if (!registry || registry[0] === undefined) return;
+
+    let einArray = registry[0]
+      .map((org: RegistryEntry | undefined) => {
+        return org?.EIN;
+      })
+      .filter(Boolean);
+
+    if (einArray.length === 0)
+      throw Error(
+        'No organizations found in registry. Please refer to repo and contact package maintainer'
+      );
+
+    return einArray;
+  } catch (error: any) {
+    return Error(error?.msg);
+  }
+};
+
+/**
+ * getAddressArrayRegistry
+ *
+ * @param {string} json - Total registry including document head
+ *
+ * @return { string[] } - Array of registry xrpl addresses
+ */
+export const getAddressArrayRegistry = (json: Registry) => {
+  try {
+    const registry = json
+      .map((obj: RegistryArray | Head) => {
+        if ('registry' in obj) return obj.registry;
+        return undefined;
+      })
+      .filter(Boolean);
+
+    if (!registry || registry[0] === undefined) return;
+
+    let addressArray = registry[0]
+      .map((org: RegistryEntry | undefined) => {
+        return org?.address;
+      })
+      .filter(Boolean);
+
+    if (addressArray.length === 0)
+      throw Error(
+        'No organizations found in registry. Please refer to repo and contact package maintainer'
+      );
+
+    return addressArray;
+  } catch (error: any) {
+    return Error(error?.msg);
+  }
+};
 
 /**
  *
+ * determineVersionOfRegistry
  *
- *
+ * @return {string} - version of registry
  */
-export const getEINArrayRegistry = () => {};
-
-/**
- *
- *
- *
- */
-export const determineVersionOfRegistry = () => {};
+export const determineVersionOfRegistry = () => {
+  let packageName = '@thebettermint/registry';
+  let whyBuffer = spawnSync('yarn', ['why', packageName]);
+  let grepBuffer = spawnSync('grep', ['Found'], { input: whyBuffer.stdout });
+  let outputArray = grepBuffer.stdout.toString().split('\n');
+  let parsedOutputArray = outputArray
+    .filter((output) => output.length > 0)
+    .map((output) => output.split('@')[1].replace('"', ''));
+  return parsedOutputArray[0];
+};
