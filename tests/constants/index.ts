f09d05dece76affb57c4c36d5a08e9fbebf0a2a7
interc0der <intercoder@whirled.io>
2022-06-21 14:44:19 -0700

    Create: Test registry and test constants

---
+++ tests/constants/index.ts
@@ -0,0 +1,32 @@
+export const registryAddresses = [
+  'rpfBYsmNBB7Y6z7qHS8g26KE3y3hHaTxkq',
+  'rpfBYsmNBB7Y6z7qHS8g26KE3y3hHaTxkq',
+  'rpfBYsmNBB7Y6z7qHS8g26KE3y3hHaTxkq',
+  'rpfBYsmNBB7Y6z7qHS8g26KE3y3hHaTxkq',
+];
+
+export const testServerURL = 'wss://s1.ripple.com/';
+export const testAddress = 'rpfBYsmNBB7Y6z7qHS8g26KE3y3hHaTxkq';
+
+export const testOrganization = {
+  name: 'Remedy Alliance/For the People',
+  description: 'Providing naloxone for harm reduction programs around the USA',
+  address: 'rpfBYsmNBB7Y6z7qHS8g26KE3y3hHaTxkq',
+  image:
+    'https://github.com/thebettermint/registry/blob/a3fc4bad1c3eee86f52ed4eb2a873e54fbd8855a/src/assets/sample_logo.png',
+  url: '',
+  phone: '',
+  mailingAddress: '',
+  country: '',
+  EIN: '87-3486445',
+  initatives: [
+    {
+      title: 'Remedy #1',
+      description: 'Test initiative',
+      tag: 123456,
+      XAddress: '',
+      start: 1655656869,
+      end: 1655656869,
+    },
+  ],
+};
