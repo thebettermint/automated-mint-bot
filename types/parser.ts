f03946c8ceb43fbefc540c6c0cd50f3dc2b2698d
interc0der <intercoder@whirled.io>
2022-06-22 00:34:54 -0700

    Create: add type definitions for transaction parsing

---
+++ types/parser.ts
@@ -0,0 +1,34 @@
+import { TxResponse, TransactionStream, TransactionEntryResponse } from 'xrpl';
+
+export type modTxResponse = TxResponse;
+
+export type modTransactionStream = TransactionStream;
+
+export type modTransactionEntryResponse = TransactionEntryResponse;
+
+export type TxParserInterface =
+  | modTxResponse
+  | modTransactionStream
+  | modTransactionEntryResponse;
+
+export interface PaymentInterface extends Object {
+  source?: string;
+  destination?: string;
+  source_balance_changes: { currency?: string }[];
+  destination_balance_changes?: string[];
+  currency?: string;
+  amount?: string;
+  destination_tag?: number;
+  source_tag?: number;
+  invoice_id?: string;
+  delivered_amount?: string;
+  max_amount?: string;
+  source_currency?: string;
+  issuer?: string;
+  fee?: string;
+  ledger_index?: Number;
+  tx_index?: Number;
+  time?: Number;
+  tx_hash?: string;
+  client?: string;
+}
