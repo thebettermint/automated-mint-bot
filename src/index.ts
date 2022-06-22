b0976a7bff7e9be7b833cd1da3d62df3da7891d9
interc0der <intercoder@whirled.io>
2022-06-22 00:35:42 -0700

    Update: Modified source file for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,42 @@
 import xrplSubscriptionToRegistryWS from './lib/ws';
-import { serverURL } from './lib/constants';
+import { /* serverURL, */ mainServerURL } from './lib/constants';
 import json from '@thebettermint/registry/src/registry.json';
 import lib from './lib';
 import { wsStatusMessages } from './lib/constants';
 import { TransactionStream } from 'xrpl';
 
+import parse from './lib/parse';
+
 const main = () => {
-  let registryAddresses = lib.registry.getAddressArrayRegistry(json);
+  const registryAddresses = lib.registry.getAddressArrayRegistry(json);
   if (registryAddresses instanceof Error) return;
 
   let subscribe = new xrplSubscriptionToRegistryWS({
-    url: serverURL,
+    url: mainServerURL,
     registry: registryAddresses,
-    test: false,
+    test: true,
   });
 
   subscribe.ws?.once(wsStatusMessages.connected, () => {
     console.log(wsStatusMessages.connected);
   });
 
-  subscribe.ws?.once(wsStatusMessages.tx, (event: TransactionStream) => {
-    console.log(wsStatusMessages.tx);
-    console.log(event);
+  subscribe.ws?.addListener(wsStatusMessages.tx, (event: TransactionStream) => {
+    //console.log(wsStatusMessages.tx);
+    let handled: any = parse.txHandler(event);
+    let parsed = parse.allPayments(handled);
+    if (!parsed) return;
+
+    console.log(parsed);
+    if (
+      !parsed.destination ||
+      registryAddresses?.indexOf(parsed.destination) === -1
+    )
+      return;
+
+    let index = registryAddresses?.indexOf(parsed.destination);
+    console.log(index);
+    console.log(parsed);
 
     //TO DO
     // Parse transactions and throw away all types except for validated payment types
