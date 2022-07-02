b0d7859a22353f8a09cee7b200aa6318d232f62b
interc0der <intercoder@whirled.io>
2022-07-01 23:54:00 -0700

    Update: src with main mint bot logic

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,57 @@
-import xrplSubscriptionToRegistryWS from './lib/ws';
-import { /* serverURL, */ mainServerURL } from './lib/constants';
 import json from '@thebettermint/registry/src/registry.json';
 import lib from './lib';
-import { wsStatusMessages } from './lib/constants';
-import { TransactionStream } from 'xrpl';
 
+import Client, { constants } from '@thebettermint/xrpl-tx-parser';
+import config from '../config/config.json';
 import parse from './lib/parse';
+import ipfs from './lib/utils/ipfs';
 
 const main = () => {
   const registryAddresses = lib.registry.getAddressArrayRegistry(json);
+  console.log(registryAddresses);
   if (registryAddresses instanceof Error) return;
 
-  let subscribe = new xrplSubscriptionToRegistryWS({
-    url: mainServerURL,
+  let api = new Client({
+    url: config[0].network,
     registry: registryAddresses,
-    test: true,
+    reconnect: true,
   });
 
-  subscribe.ws?.once(wsStatusMessages.connected, () => {
-    console.log(wsStatusMessages.connected);
-  });
+  let events = constants.wsStatusMessages;
 
-  subscribe.ws?.addListener(wsStatusMessages.tx, (event: TransactionStream) => {
-    //console.log(wsStatusMessages.tx);
-    let handled: any = parse.txHandler(event);
-    let parsed = parse.allPayments(handled);
-    if (!parsed) return;
+  api.once(events.connected, () => _onConnected());
+  api.on(events.tx, (e: any) => _onTx(e));
+  api.once(events.closed, () => _onClose());
+  api.on(events.reconnect, () => _onReconnect());
+  api.on(events.timeout, () => _onTimeout());
+  api.on(events.error, (evt: any[]) => _onError(evt));
+  api.on('ping', () => api.emit('pong'));
 
-    console.log(parsed);
-    if (
-      !parsed.destination ||
-      registryAddresses?.indexOf(parsed.destination) === -1
-    )
-      return;
+  const _onConnected = () => {
+    console.log(events.connected);
+  };
 
-    let index = registryAddresses?.indexOf(parsed.destination);
-    console.log(index);
-    console.log(parsed);
+  const _onTx = (e: any) => {
+    let meta = parse.TXtoCreditMetadata(e);
+    if (meta) ipfs.handleUploadToIpfs(meta);
+    return;
+  };
 
-    //TO DO
-    // Parse transactions and throw away all types except for validated payment types
-    // Check if destination matches one of the registry accounts
-    // If false -> return
-    // If true ->
-    // check the type of token ( issuer / currency ) of the payment
-    // check if on the list of approved currencies ( default XRP )
-    // check if the donation amount is greater than minimum limit ( default 5 XRP )
-    //      note: this is to avoid and filter out spam donations
-    // check to see if organization account destination tag has enough to mint
-    // if true -> mint token and send to donors account
-  });
+  const _onTimeout = () => {
+    console.log('timeout');
+  };
+
+  const _onReconnect = () => {
+    console.log('reconnect');
+  };
+
+  const _onError = (e: any) => {
+    console.log(e);
+  };
+
+  const _onClose = () => {
+    console.log(events.closed);
+  };
 };
 
 main();
