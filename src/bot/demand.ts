18e9c1e21a83b0b75058bed9120dd3b74dd0bf68
interc0der <intercoder@whirled.io>
2022-09-03 14:14:26 -0700

    Add: bot functionality to its own folder

---
+++ src/bot/demand.ts
@@ -0,0 +1,76 @@
+import config from '../../config';
+import ipfs from '../lib/ipfs';
+import x from '../lib/xrpl';
+import apexService from '../db/services/apex.service';
+import path from 'path';
+import { processQRCode } from '../lib/qr';
+import { processOverlay } from '../lib/sharp';
+import uuid from 'node-uuid';
+
+import { wsServer } from '../bot/monitor/ws';
+
+const assetPath: any = {
+  mock1: path.resolve(config.directory, 'src/api/assets/images/mock1.png'),
+  mock2: path.resolve(config.directory, 'src/api/assets/images/mock2.png'),
+};
+
+const onDemand = async ({ address }: { address: string }) => {
+  const api = await x.client.init(config.nodes.xrplnft.wss);
+
+  console.log(wsServer.peerSockets);
+
+  try {
+    let dbAsset = await apexService.add({
+      publicAddress: address,
+    });
+
+    let assetKeys = Object.keys(assetPath);
+    let selectionPath = assetKeys[Math.floor(Math.random() * assetKeys.length)];
+
+    let qrData = await processQRCode({
+      address: address,
+      uuid: dbAsset.uuid,
+      issuer: config.wallet.address,
+      createdAt: dbAsset.createdAt,
+    });
+
+    // Process image to generate nft
+    let processImage = await processOverlay(assetPath[selectionPath], qrData);
+
+    // upload image and meta data to ipfs
+    let hash = await ipfs.handleUploadToIpfs(processImage);
+    console.log(`IPFS Upload Successful. CID: ${hash}`);
+
+    await api.connect();
+
+    let nft = await x.nftCreate({ api: api, uri: `ipfs://${hash}` });
+    if (!nft || nft instanceof Error) throw Error('Error @ Mint');
+
+    let offerId = await x.nftTransfer({
+      api: api,
+      destination: address,
+      id: nft[1],
+    });
+
+    if (!offerId || offerId instanceof Error)
+      throw Error('Error @ OfferCreate');
+
+    dbAsset.tokenId = nft[1];
+    dbAsset.offerId = offerId;
+    dbAsset.state = 'offered';
+    dbAsset.updatedAt = new Date(Date.now());
+
+    await dbAsset.save();
+
+    wsServer.sendAll(dbAsset);
+
+    return dbAsset;
+  } catch (error: any) {
+    console.log(error);
+    return Error(error.message);
+  } finally {
+    api.disconnect();
+  }
+};
+
+export default onDemand;
---
+++ src/bot/monitor/ws.ts
@@ -0,0 +1,145 @@
+import config from '../../../config';
+
+export class WS {
+  wss: any;
+  peerSockets: any;
+  constructor() {
+    const WebSocket = require('ws');
+    this.peerSockets = {};
+    this.wss = new WebSocket.Server({ port: config.ws.port });
+    this.wss.on('connection', (socket: any) =>
+      this._onConnection(new Peer(socket))
+    );
+    console.log('The websocket is running and listening for requests');
+  }
+
+  _onConnection = (peer: any) => {
+    peer.socket.on('message', (message: any) => this._onMessage(peer, message));
+    this._keepAlive(peer);
+    console.log(`New peer ( ${peer.id} )`);
+
+    // send displayName
+    this._send(peer, {
+      type: 'success',
+      msg: 'The connection to the server websocket was successful',
+      id: peer.id,
+    });
+  };
+
+  _onMessage = async (sender: any, message: any) => {
+    // Try to parse message
+    try {
+      message = JSON.parse(message);
+    } catch (e) {
+      return; // TODO: handle malformed JSON
+    }
+
+    switch (message.type) {
+      case 'pong':
+        sender.lastBeat = Date.now();
+        break;
+      case 'subscribe':
+        sender.lastBeat = Date.now();
+        if (this.peerSockets[sender.id]) this._removePeerSockets(sender);
+        this.peerSockets[sender.id] = sender;
+        break;
+    }
+  };
+
+  sendAll = (message: any) => {
+    if (!message) return;
+    if (this.wss.readyState !== this.wss.OPEN) return;
+
+    message = JSON.stringify(message);
+
+    let keys = Object.keys(this.peerSockets);
+    if (!keys || keys.length === 0) return;
+    keys.map((id: any) => {
+      this.peerSockets[id].send(message, (error: any) => console.log(error));
+    });
+  };
+
+  _send = (peer: any, message: any) => {
+    if (!peer) return;
+    if (this.wss.readyState !== this.wss.OPEN) return;
+    message = JSON.stringify(message);
+    peer.socket.send(message, (error: any) =>
+      console.log(error, 'error sending message to peer')
+    );
+  };
+
+  _removePeerSockets = (peer: any) => {
+    console.log(`Removing peer from socket( ${peer.id} )`);
+    //this.peerSockets[peer.id].forEach((socket: any) => socket.disconnect());
+    delete this.peerSockets[peer.id];
+  };
+
+  _keepAlive = (peer: any) => {
+    this._cancelKeepAlive(peer);
+    var timeout = 30000;
+    if (!peer.lastBeat) {
+      peer.lastBeat = Date.now();
+    }
+    if (Date.now() - peer.lastBeat > 2 * timeout) {
+      this._removePeerSockets(peer);
+      setTimeout(() => {
+        peer.socket.terminate();
+      }, 5000);
+      return;
+    }
+
+    this._send(peer, { type: 'ping' });
+
+    peer.timerId = setTimeout(() => this._keepAlive(peer), timeout);
+  };
+
+  _cancelKeepAlive = (peer: any) => {
+    if (peer && peer.timerId) {
+      clearTimeout(peer.timerId);
+    }
+  };
+}
+
+class Peer {
+  socket: any;
+  id: string;
+  timerId: number;
+  lastBeat: number;
+  constructor(socket: any) {
+    // set socket
+    this.socket = socket;
+    this.id = this._uuid();
+    // for keepalive
+    this.timerId = 0;
+    this.lastBeat = Date.now();
+  }
+
+  _uuid() {
+    let uuid = '',
+      ii;
+    for (ii = 0; ii < 32; ii += 1) {
+      switch (ii) {
+        case 8:
+        case 20:
+          uuid += '-';
+          uuid += ((Math.random() * 16) | 0).toString(16);
+          break;
+        case 12:
+          uuid += '-';
+          uuid += '4';
+          break;
+        case 16:
+          uuid += '-';
+          uuid += ((Math.random() * 4) | 8).toString(16);
+          break;
+        default:
+          uuid += ((Math.random() * 16) | 0).toString(16);
+      }
+    }
+    return uuid;
+  }
+}
+
+export const wsServer = new WS();
+
+wsServer;
